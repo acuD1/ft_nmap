dcee7b0e6dccca617d4ea7ac7b14f361586241b8
Cedric M'Passi <cempassi@student.42.fr>
2021-06-25 16:13:05 +0200

    Add init and defaults (#46)
    * Add init and defaults
    
    * Set correct types

---
+++ srcs/init.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/25 15:09:07 by cempassi          #+#    #+#             */
+/*   Updated: 2021/06/25 16:11:25 by cempassi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_nmap.h"
+
+void set_defaults(t_nmap *nmap)
+{
+    ft_bzero(&nmap, sizeof(t_nmap));
+    ft_bzero(&nmap->target_ipv4, sizeof(nmap->target_ipv4));
+    nmap->threads = DEFAULT_THREADS;
+    nmap->scan = DEFAULT_SCAN;
+    nmap->ports = NULL;
+}
+
+int init_nmap(t_nmap *nmap, int argc, char **argv)
+{
+    set_defaults(nmap);
+    if (set_opts_args(nmap, argc, argv) != SUCCESS)
+        exit_routine(nmap, FAILURE);
+    return (SUCCESS);
+}
