ba06e4b5a4d33a3b6ff83575e93b99f31502ea28
acuD1 <arsciand@student.42.fr>
2021-07-16 14:45:34 +0200

    Fix msg for getaddrinfo() handler

--- srcs/errors.c
+++ srcs/errors.c
@@ -6,7 +6,7 @@
 /*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 13:15:56 by arsciand          #+#    #+#             */
-/*   Updated: 2021/06/25 14:36:03 by arsciand         ###   ########.fr       */
+/*   Updated: 2021/07/16 14:45:03 by arsciand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,27 @@ void     getaddrinfo_error_handler(char *arg, int status)
     {
         case EAI_ADDRFAMILY:
             dprintf(STDERR_FILENO,
-                "ft_ping: %s: Address family for hostname not supported\n",
+                "ft_nmap: %s: Address family for hostname not supported\n",
                 arg);
             break ;
         case EAI_AGAIN:
             dprintf(STDERR_FILENO,
-                "ft_ping: %s: Temporary failure in name resolution\n",
+                "ft_nmap: %s: Temporary failure in name resolution\n",
                 arg);
             break ;
         case EAI_NODATA:
             dprintf(STDERR_FILENO,
-                "ft_ping: %s: No address associated with hostname\n",
+                "ft_nmap: %s: No address associated with hostname\n",
                 arg);
             break ;
         case EAI_NONAME:
             dprintf(STDERR_FILENO,
-                "ft_ping: %s: Name or service not known\n",
+                "ft_nmap: %s: Name or service not known\n",
                 arg);
             break ;
         case EAI_SYSTEM:
             dprintf(STDERR_FILENO,
-                "ft_ping: %s: System error\n",
+                "ft_nmap: %s: System error\n",
                 arg);
             break ;
     }
