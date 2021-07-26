08c5742fcaff786c958cb3721326eb4a2a04bbd7
Cedric M'Passi <cedric.mpassi@pmu.fr>
2021-07-26 15:54:16 +0000

    Ajout ft_tablen

--- libft/includes/other.h
+++ libft/includes/other.h
@@ -6,7 +6,7 @@
 /*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/23 03:12:18 by cempassi          #+#    #+#             */
-/*   Updated: 2021/07/12 10:14:45 by cempassi         ###   ########.fr       */
+/*   Updated: 2021/07/26 12:45:11 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@ bool				ft_isdigit(int c);
 bool				ft_isprint(int c);
 int	    		    ft_tolower(int c);
 int					ft_toupper(int c);
+int                 ft_tablen(char **tab);
 
 #endif
