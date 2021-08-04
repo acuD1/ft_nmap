923217e7821d8147b43b81a1521c5d9fb4ecd591
acuD1 <arsciand@student.42.fr>
2021-08-04 21:55:30 +0200

    Fix non checked condition for now

--- srcs/exec_nmap.c
+++ srcs/exec_nmap.c
@@ -6,7 +6,7 @@
 /*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 12:30:31 by arsciand          #+#    #+#             */
-/*   Updated: 2021/08/02 10:02:02 by cempassi         ###   ########.fr       */
+/*   Updated: 2021/08/04 21:36:35 by arsciand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void    exec_nmap(t_nmap *nmap)
 {
-
-    if (nmap->options)
+    // if (nmap->options) // Seems broken ?
     ft_lstiter_ctx(nmap->target, nmap, send_target);
 }
