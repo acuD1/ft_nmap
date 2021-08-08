91f26269ff0f89c325ddeed360710b0289f5ae16
acuD1 <arsciand@student.42.fr>
2021-08-08 17:26:02 +0200

    Add pthread gatherer function

--- srcs/exec_nmap.c
+++ srcs/exec_nmap.c
@@ -34,6 +34,23 @@ static void threads_generator(t_nmap *nmap, pthread_t *nmap_threads)
         }
     }
 }
+
+static void threads_gatherer(t_nmap *nmap, pthread_t *nmap_threads)
+{
+    int status = 0;
+
+    for (size_t thread_id = 0; thread_id < nmap->threads; thread_id++)
+    {
+        if ((status = pthread_join(nmap_threads[thread_id], NULL)) != SUCCESS)
+        {
+            dprintf(STDERR_FILENO, "ft_nmap: pthread_join(): %s", strerror(status));
+            ft_memdel((void *)&nmap_threads);
+            exit_routine(nmap, FAILURE);
+        }
+        dprintf(STDERR_FILENO, "[DEBUG] Thread -%zu- terminated !\n", thread_id);
+    }
+}
+
 void        exec_nmap(t_nmap *nmap)
 {
     pthread_t   *nmap_threads = NULL;
@@ -55,5 +72,6 @@ void        exec_nmap(t_nmap *nmap)
     //     ft_lstiter_ctx(nmap->target, nmap, send_target);
 
     threads_generator(nmap, nmap_threads);
+    threads_gatherer(nmap, nmap_threads);
     ft_memdel((void *)&nmap_threads);
 }
