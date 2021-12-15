3c662aca3e5ce0af1aac2790e9513580298b7218
Cedric M'PASSI <cempassi@student.42.fr>
2021-12-15 22:54:10 +0000

    Format exec_nmap.c

--- srcs/exec_nmap.c
+++ srcs/exec_nmap.c
@@ -6,23 +6,25 @@
 /*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 12:30:31 by arsciand          #+#    #+#             */
-/*   Updated: 2021/08/15 14:10:14 by arsciand         ###   ########.fr       */
+/*   Updated: 2021/12/15 22:50:13 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nmap.h"
 
-void    exec_nmap(t_nmap *nmap)
+void exec_nmap(t_nmap *nmap)
 {
     if (getuid() != 0)
     {
         dprintf(STDERR_FILENO, "ft_nmap: socket: Operation not permitted\n");
         exit_routine(nmap, FAILURE);
     }
-    dprintf(STDOUT_FILENO, "[DEBUG] SOURCE IP\t\t\t-> |%s|\n",
-        inet_ntoa(((struct sockaddr_in *)&nmap->local)->sin_addr));
+
+    /* Display information */
+    print_source_ip(nmap);
     ft_lstiter(nmap->targets, print_target);
 
+    /* Portscan */
     if (nmap->options)
-    ft_lstiter_ctx(nmap->targets, nmap, send_target);
+        ft_lstiter_ctx(nmap->targets, nmap, send_target);
 }
