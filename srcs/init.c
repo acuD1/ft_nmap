a75ff6731927036015d0ead9198df17c4c2e21d0
Cedric M'PASSI <cempassi@student.42.fr>
2021-12-15 22:54:31 +0000

    Remove printf buffing

--- srcs/init.c
+++ srcs/init.c
@@ -6,23 +6,23 @@
 /*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/25 15:09:07 by cempassi          #+#    #+#             */
-/*   Updated: 2021/08/22 13:44:41 by arsciand         ###   ########.fr       */
+/*   Updated: 2021/12/15 22:44:25 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nmap.h"
 #include <unistd.h>
+#include <stdio.h>
 #include <sys/types.h>
-
 
 static void set_defaults(t_nmap *nmap)
 {
     ft_bzero(nmap, sizeof(t_nmap));
-    nmap->threads = NULL;
 }
 
 void     init_nmap(t_nmap *nmap, int argc, char **argv)
 {
+    setbuf(stdout, NULL);
     set_defaults(nmap);
 
     if (set_opts_args(nmap, argc, argv) != SUCCESS)
