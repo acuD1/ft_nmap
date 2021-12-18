9513cf62442457421df8cb22d1393af1ff1dccda
Cedric M'PASSI <cempassi@student.42.fr>
2021-12-18 16:06:45 +0000

    Correct ft_lstfold ordering

--- libft/srcs/list/ft_lstfold.c
+++ libft/srcs/list/ft_lstfold.c
@@ -6,17 +6,17 @@
 /*   By: cedricmpassi <cempassi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 01:12:02 by cedricmpa         #+#    #+#             */
-/*   Updated: 2020/10/28 01:16:29 by cedricmpa        ###   ########.fr       */
+/*   Updated: 2021/12/18 15:30:43 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "list.h"
 
-void ft_lstfold(t_list *lst, void *acc, void (*f)(void *acc, void *data))
+void ft_lstfold(t_list *lst, void *acc, void (*f)(void *data, void *acc))
 {
 	while(lst)
 	{
-		f(acc, lst->data);
+		f(lst->data, acc);
 		lst = lst->next;
 	}
 }
