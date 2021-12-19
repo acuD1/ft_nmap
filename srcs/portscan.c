5f7b8a17f500643eb858323646a2b3adca59e304
Cedric M'PASSI <cempassi@student.42.fr>
2021-12-19 17:37:26 +0000

    Add dispatch ports to threads

--- srcs/portscan.c
+++ srcs/portscan.c
@@ -6,12 +6,13 @@
 /*   By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 23:27:15 by cempassi          #+#    #+#             */
-/*   Updated: 2021/12/18 18:36:37 by cempassi         ###   ########.fr       */
+/*   Updated: 2021/12/19 17:35:31 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nmap.h"
 #include "list.h"
+#include <pthread.h>
 #include <stdio.h>
 #include <strings.h>
 
@@ -22,7 +23,6 @@
 static void init_thread(t_nmap *nmap, t_target *target, t_thread *thread)
 {
     thread->ports = NULL;
-    thread->id = 0;
     thread->src = nmap->src;
     thread->dst = target->dst;
 }
@@ -69,10 +69,52 @@ static void set_range(t_port *target, uint16_t *min_port, uint16_t *max_port)
     }
 }
 
+static uint8_t dispatch_leftovers(t_list *ports, t_list *threads,
+                                  uint16_t min_port)
+{
+    t_list * current_port = ports;
+    uint16_t holder;
+    uint16_t max_port = 0;
+    t_list   *tmp;
+
+    tmp = threads;
+    while(tmp && ports)
+    {
+        t_port *target_port = ports->data;
+        if (target_port->type == E_PORT_SINGLE)
+        {
+            t_thread *thread = tmp->data;
+            if (add_port(&thread->ports, target_port->data.port) == FAILURE)
+                return (FAILURE);
+        }
+        else
+        {
+            holder = min_port;
+            set_range(target_port, &min_port, &max_port);
+            if (current_port == ports)
+                min_port = holder;
+            while (min_port <= max_port && tmp)
+            {
+                t_thread *thread = tmp->data;
+                if (add_port(&thread->ports, min_port) == FAILURE)
+                    return (FAILURE);
+                tmp = tmp->next;
+                min_port++;
+            }
+            ports = ports->next;
+            continue;
+        }
+        tmp = tmp->next;
+        ports = ports->next;
+    }
+    return (SUCCESS);
+}
+
 static t_list *generate_threads(t_target *target, t_thread *thread_template)
 {
     t_list * threads;
     uint16_t port_nbr = 0;
+    uint16_t dispatched_ports = 0;
     uint16_t min_port;
     uint16_t max_port;
 
@@ -80,42 +122,76 @@ static t_list *generate_threads(t_target *target, t_thread *thread_template)
 
     for (t_list *current = target->ports; current; current = current->next)
     {
-        t_port *target_port = current->data;
-        if (target_port->type == E_PORT_SINGLE)
+        if (dispatched_ports >= target->port_nbr - target->port_leftover)
         {
-            port_nbr++;
-            if (add_port(&thread_template->ports, target_port->data.port)
-                == FAILURE)
+            if (dispatch_leftovers(current, threads, 0) == FAILURE)
                 return (NULL);
         }
         else
         {
-            set_range(target_port, &min_port, &max_port);
-            while (min_port <= max_port)
+            t_port *target_port = current->data;
+            if (target_port->type == E_PORT_SINGLE)
             {
                 port_nbr++;
-                if (add_port(&thread_template->ports, min_port) == FAILURE)
+                dispatched_ports++;
+                if (add_port(&thread_template->ports, target_port->data.port)
+                    == FAILURE)
                     return (NULL);
                 if (port_nbr == target->port_per_thread)
                 {
                     if (create_thread(&threads, thread_template) == SUCCESS)
                         port_nbr = 0;
                     else
+                    {
+                        //FIXME: Free previous on failure
                         return (NULL);
+                    }
                 }
-                min_port++;
             }
-        }
-
-        if (port_nbr == target->port_per_thread)
-        {
-            if (create_thread(&threads, thread_template) == SUCCESS)
-                port_nbr = 0;
             else
-                return (NULL);
+            {
+                set_range(target_port, &min_port, &max_port);
+                while (min_port <= max_port)
+                {
+                    port_nbr++;
+                    dispatched_ports++;
+                    if (add_port(&thread_template->ports, min_port) == FAILURE)
+                        return (NULL);
+                    if (port_nbr == target->port_per_thread)
+                    {
+                        if (create_thread(&threads, thread_template) == SUCCESS)
+                            port_nbr = 0;
+                        else
+                        {
+                            //FIXME: Free previous on failure
+                            return (NULL);
+                        }
+                    }
+                    min_port++;
+                    if (dispatched_ports >= target->port_nbr - target->port_leftover)
+                    {
+                        if (dispatch_leftovers(current, threads, min_port) == FAILURE)
+                            return (NULL);
+                        return(threads);
+                    }
+                }
+            }
         }
     }
     return (threads);
+}
+
+static void *scan_thread(void *data)
+{
+    t_thread *thread;
+
+    thread = data;
+    pthread_mutex_lock(&g_lock);
+    printf("[DEBUG IN THREAD] THREAD ID: %lu\n", pthread_self());
+    print_thread(data);
+    printf("--------------------- END OF THREAD --------------------- \n");
+    pthread_mutex_unlock(&g_lock);
+    pthread_exit(SUCCESS);
 }
 
 int scan_target(void *data, void *context)
@@ -123,19 +199,56 @@ int scan_target(void *data, void *context)
     t_target *target;
     t_nmap *  nmap;
     t_list *  threads;
-    t_thread  thread;
+    t_list *  threads_id;
+
+    t_thread thread_data_template;
 
     nmap = context;
     target = data;
     threads = NULL;
+    threads_id = NULL;
     print_target(target);
 
-    // Thread repartition must happend here
-    init_thread(nmap, target, &thread); // Set base values
-    if ((threads = generate_threads(target, &thread)) == NULL)
+    // 1: Ports repartition between treads
+    init_thread(nmap, target, &thread_data_template); // Set base values
+    if ((threads = generate_threads(target, &thread_data_template)) == NULL)
         return (FAILURE);
-    ft_lstiter(threads, print_thread);
 
+    // 2: Launch threads and collect their identifiers in a list of pthread_t
+    for (t_list *tmp = threads; tmp; tmp = tmp->next)
+    {
+        // Thread identifier
+        pthread_t tid = 0;
+        int       result;
+
+        result = pthread_create(&tid, NULL, scan_thread, tmp->data);
+        if (result != SUCCESS
+            || ft_lstaddback(&threads_id, ft_lstnew(&tid, sizeof(pthread_t)))
+                != SUCCESS)
+        {
+            ft_lstdel(&threads, delete_thread);
+            ft_lstdel(&threads_id, NULL);
+            return (FAILURE);
+        }
+    }
+    // 3: Join on each launched thread
+
+    // 4: Cleanup and exit
+
+    for (t_list *tmp = threads_id; tmp; tmp = tmp->next)
+    {
+        pthread_t *tid;
+        int        result;
+
+        tid = tmp->data;
+        result = pthread_join(*tid, NULL);
+        if (result != SUCCESS)
+        {
+            ft_lstdel(&threads, delete_thread);
+            ft_lstdel(&threads_id, NULL);
+        }
+    }
     ft_lstdel(&threads, delete_thread);
+    ft_lstdel(&threads_id, NULL);
     return (SUCCESS);
 }
