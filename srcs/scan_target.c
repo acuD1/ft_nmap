7eec2ba69ea997ae6bbb8abd2843ffba9775d2db
acuD1 <arsciand@student.42.fr>
2022-01-12 10:26:36 +0000

    Refacto function clean joined threads

--- srcs/scan_target.c
+++ srcs/scan_target.c
@@ -6,7 +6,7 @@
 /*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 23:27:15 by cempassi          #+#    #+#             */
-/*   Updated: 2022/01/11 10:45:59 by arsciand         ###   ########.fr       */
+/*   Updated: 2022/01/12 10:26:03 by arsciand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,24 @@ static uint8_t  close_fds(t_thread *thread)
     return (SUCCESS);
 }
 
+/* In case of sigint when creating threads, we need to join all threads
+before cleanup */
+uint8_t         cleaner_joined_thread(t_list *threads, t_list *threads_id)
+{
+    for (t_list *tmp = threads_id; tmp; tmp = tmp->next)
+    {
+        pthread_t   *tid = tmp->data;
+
+        if (pthread_join(*tid, NULL) != SUCCESS)
+            dprintf(STDERR_FILENO,
+                "ft_nmap: pthread_join(): Error joining thread %lu\n", *tid);
+    }
+
+    ft_lstdel(&threads, delete_thread);
+    ft_lstdel(&threads_id, NULL);
+    return (FAILURE);
+}
+
 int             scan_target(void *data, void *context)
 {
     t_nmap      *nmap       = context;
@@ -91,42 +109,22 @@ int             scan_target(void *data, void *context)
         return (FAILURE);
     }
 
-
     // 2: Launch threads and collect their identifiers in a list of pthread_t
     for (t_list *tmp = threads; tmp; tmp = tmp->next)
     {
         pthread_t   tid = 0;
 
         if (g_nmap.is_canceld == TRUE)
-        {
-
-            // Handle panic join if SIGINT
-            for (t_list *tmp_2 = threads_id; tmp_2; tmp_2 = tmp_2->next)
-            {
-                pthread_t   *tid_2 = tmp_2->data;
-
-                if (pthread_join(*tid_2, NULL) != SUCCESS)
-                    dprintf(STDERR_FILENO,
-                        "ft_nmap: pthread_join(): Error joining thread %lu\n",
-                        *tid_2);
-            }
-
-            ft_lstdel(&threads, delete_thread);
-            ft_lstdel(&threads_id, NULL);
-            return (FAILURE);
-        }
+            return (cleaner_joined_thread(threads, threads_id));
 
         if (pthread_create(&tid, NULL, scan_thread, tmp->data) != SUCCESS
             || ft_lstaddback(&threads_id, ft_lstnew(&tid, sizeof(pthread_t)))
                 != SUCCESS)
         {
             dprintf(STDERR_FILENO, "ft_nmap: scan_target(): Error creating thread\n");
-            ft_lstdel(&threads, delete_thread);
-            ft_lstdel(&threads_id, NULL);
-            return (FAILURE);
+            return (cleaner_joined_thread(threads, threads_id));
         }
     }
-
 
     // 3: Join on each launched thread
     for (t_list *tmp = threads_id; tmp; tmp = tmp->next)
